a9bf73a3b307657243b93e1fad09efc203d1609b
karvaroz <vargasriosk@gmail.com>
2022-06-24 10:31:08 -0500

    initial setup

---
+++ src/data/Questions.js
@@ -0,0 +1,711 @@
+export const Questions = [
+  {
+    id: 1,
+    category: "Animales",
+    difficulty: "Muy Fácil",
+    question: "¿Cuál es el nombre científico de los humanos modernos?",
+    options: [
+      {
+        id: 101,
+        text: "Homo Ergaster",
+        isCorrect: false,
+      },
+      {
+        id: 102,
+        text: "Homo Sapiens",
+        isCorrect: true,
+      },
+      {
+        id: 103,
+        text: "Homo Erectus",
+        isCorrect: false,
+      },
+      {
+        id: 104,
+        text: "Homo Neanderthalensis",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    category: "Animales",
+    difficulty: "Muy Fácil",
+    question: "¿Hippocampus es el nombre en latín de la criatura marina?",
+    options: [
+      {
+        id: 201,
+        text: "Caballo de mar",
+        isCorrect: true,
+      },
+      {
+        id: 202,
+        text: "Delfín",
+        isCorrect: false,
+      },
+      {
+        id: 203,
+        text: "Ballena",
+        isCorrect: false,
+      },
+      {
+        id: 204,
+        text: "Pulpo",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    category: "Animales",
+    difficulty: "Muy Fácil",
+    question: "¿Cuantas patas tienen las mariposas?",
+    options: [
+      {
+        id: 301,
+        text: "0",
+        isCorrect: false,
+      },
+      {
+        id: 302,
+        text: "2",
+        isCorrect: true,
+      },
+      {
+        id: 303,
+        text: "4",
+        isCorrect: false,
+      },
+      {
+        id: 304,
+        text: "6",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    category: "Animales",
+    difficulty: "Muy Fácil",
+    question: "Por definición, ¿dónde vive un animal abisopelágico?",
+    options: [
+      {
+        id: 401,
+        text: "Dentro de un árbol",
+        isCorrect: false,
+      },
+      {
+        id: 402,
+        text: "En el fondo del océano",
+        isCorrect: true,
+      },
+      {
+        id: 403,
+        text: "En el desierto",
+        isCorrect: false,
+      },
+      {
+        id: 404,
+        text: "En la cima de una montaña",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 5,
+    category: "Animales",
+    difficulty: "Muy Fácil",
+    question: "¿Cuantos dientes tiene un conejo adulto?",
+    options: [
+      {
+        id: 501,
+        text: "30",
+        isCorrect: false,
+      },
+      {
+        id: 502,
+        text: "24",
+        isCorrect: false,
+      },
+      {
+        id: 503,
+        text: "28",
+        isCorrect: true,
+      },
+      {
+        id: 505,
+        text: "26",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 6,
+    category: "Mitología",
+    difficulty: "Fácil",
+    question:
+      "¿Quién en la mitología griega, que dirigió a los argonautas en busca del vellocino de oro?",
+    options: [
+      {
+        id: 601,
+        text: "Odiseo",
+        isCorrect: false,
+      },
+      {
+        id: 602,
+        text: "Dédalo",
+        isCorrect: false,
+      },
+      {
+        id: 603,
+        text: "Castor",
+        isCorrect: true,
+      },
+      {
+        id: 605,
+        text: "Jason",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 7,
+    category: "Mitología",
+    difficulty: "Fácil",
+    question: "¿Quién era el rey de los dioses en la mitología griega antigua?",
+    options: [
+      {
+        id: 701,
+        text: "Poseidón",
+        isCorrect: false,
+      },
+      {
+        id: 702,
+        text: "Apolo",
+        isCorrect: false,
+      },
+      {
+        id: 703,
+        text: "Zeus",
+        isCorrect: true,
+      },
+      {
+        id: 705,
+        text: "Hermes",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    category: "Mitología",
+    difficulty: "Fácil",
+    question:
+      "¿Qué dios griego y romano era conocido como el dios de la música, la verdad y la profecía, la curación, el sol y la luz, la peste, la poesía y más?",
+    options: [
+      {
+        id: 801,
+        text: "Poseidón",
+        isCorrect: false,
+      },
+      {
+        id: 802,
+        text: "Apolo",
+        isCorrect: false,
+      },
+      {
+        id: 803,
+        text: "Zeus",
+        isCorrect: true,
+      },
+      {
+        id: 805,
+        text: "Hermes",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    category: "Mitología",
+    difficulty: "Fácil",
+    question: "¿El dios griego Poseidón era el dios de qué?",
+    options: [
+      {
+        id: 901,
+        text: "Fuego",
+        isCorrect: false,
+      },
+      {
+        id: 902,
+        text: "El Mar",
+        isCorrect: true,
+      },
+      {
+        id: 903,
+        text: "Sol",
+        isCorrect: false,
+      },
+      {
+        id: 905,
+        text: "Guerra",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    category: "Mitología",
+    difficulty: "Fácil",
+    question: "En la mayoría de las tradiciones, ¿quién fue la esposa de Zeus?",
+    options: [
+      {
+        id: 1001,
+        text: "Hestia",
+        isCorrect: false,
+      },
+      {
+        id: 1002,
+        text: "Atenea",
+        isCorrect: false,
+      },
+      {
+        id: 1003,
+        text: "Hera",
+        isCorrect: true,
+      },
+      {
+        id: 1005,
+        text: "Afrodita",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    category: "Historia",
+    difficulty: "Media",
+    question:
+      "Cuando Cristóbal Colón navegó a América, ¿cuál fue la primera región a la que llegó?",
+    options: [
+      {
+        id: 1101,
+        text: "Archipiélago de las Bahamas",
+        isCorrect: true,
+      },
+      {
+        id: 1102,
+        text: "Florida",
+        isCorrect: false,
+      },
+      {
+        id: 1103,
+        text: "Istmo de Panamá",
+        isCorrect: false,
+      },
+      {
+        id: 1105,
+        text: "Nicaragua",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    category: "Historia",
+    difficulty: "Media",
+    question:
+      "Cuando Cristóbal Colón navegó a América, ¿cuál fue la primera región a la que llegó?",
+    options: [
+      {
+        id: 1201,
+        text: "Archipiélago de las Bahamas",
+        isCorrect: true,
+      },
+      {
+        id: 1202,
+        text: "Florida",
+        isCorrect: false,
+      },
+      {
+        id: 1203,
+        text: "Istmo de Panamá",
+        isCorrect: false,
+      },
+      {
+        id: 1205,
+        text: "Nicaragua",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    category: "Historia",
+    difficulty: "Media",
+    question: "¿En qué año terminó la Primera Guerra Mundial?",
+    options: [
+      {
+        id: 1301,
+        text: "1912",
+        isCorrect: false,
+      },
+      {
+        id: 1302,
+        text: "1916",
+        isCorrect: false,
+      },
+      {
+        id: 1303,
+        text: "1914",
+        isCorrect: false,
+      },
+      {
+        id: 1305,
+        text: "1918",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 14,
+    category: "Historia",
+    difficulty: "Media",
+    question: "¿Cuál de estas instalaciones no estaba presente en el Titanic?",
+    options: [
+      {
+        id: 1401,
+        text: "Pista de squash",
+        isCorrect: false,
+      },
+      {
+        id: 1402,
+        text: "Baño turco",
+        isCorrect: false,
+      },
+      {
+        id: 1403,
+        text: "Sala de desmayos",
+        isCorrect: true,
+      },
+      {
+        id: 1405,
+        text: "Perrera",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    category: "Historia",
+    difficulty: "Media",
+    question:
+      "¿Cuál de los siguientes pueblos del antiguo Cercano Oriente todavía existe como un grupo étnico moderno?",
+    options: [
+      {
+        id: 1501,
+        text: "Babilonios",
+        isCorrect: false,
+      },
+      {
+        id: 1502,
+        text: "Asirios",
+        isCorrect: true,
+      },
+      {
+        id: 1503,
+        text: "Hititas",
+        isCorrect: false,
+      },
+      {
+        id: 1505,
+        text: "Elamitas",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 16,
+    category: "Ciencia y Naturaleza",
+    difficulty: "Media",
+    question: "¿Cuál es el color de la sangre no oxidada?",
+    options: [
+      {
+        id: 1601,
+        text: "Verde",
+        isCorrect: false,
+      },
+      {
+        id: 1602,
+        text: "Violeta",
+        isCorrect: false,
+      },
+      {
+        id: 1603,
+        text: "Azul",
+        isCorrect: false,
+      },
+      {
+        id: 1605,
+        text: "Rojo",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 17,
+    category: "Ciencia y Naturaleza",
+    difficulty: "Media",
+    question: "¿Qué elemento tiene el número atómico de 7?",
+    options: [
+      {
+        id: 1701,
+        text: "Nitrógeno",
+        isCorrect: true,
+      },
+      {
+        id: 1702,
+        text: "Oxígeno",
+        isCorrect: false,
+      },
+      {
+        id: 1703,
+        text: "Hidrógeno",
+        isCorrect: false,
+      },
+      {
+        id: 1705,
+        text: "Neón",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 18,
+    category: "Ciencia y Naturaleza",
+    difficulty: "Media",
+    question: "¿Cuántos lóbulos tiene el pulmón derecho humano?",
+    options: [
+      {
+        id: 1801,
+        text: "1",
+        isCorrect: false,
+      },
+      {
+        id: 1802,
+        text: "2",
+        isCorrect: false,
+      },
+      {
+        id: 1803,
+        text: "3",
+        isCorrect: true,
+      },
+      {
+        id: 1805,
+        text: "4",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 19,
+    category: "Ciencia y Naturaleza",
+    difficulty: "Media",
+    question:
+      "¿Cuál de los siguientes hombres no tiene un elemento químico que lleva su nombre?",
+    options: [
+      {
+        id: 1901,
+        text: "Enrico Fermi",
+        isCorrect: false,
+      },
+      {
+        id: 1902,
+        text: "Niels Bohr",
+        isCorrect: false,
+      },
+      {
+        id: 1903,
+        text: "Albert Einstein",
+        isCorrect: false,
+      },
+      {
+        id: 1905,
+        text: "Isaac Newton",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    id: 20,
+    category: "Ciencia y Naturaleza",
+    difficulty: "Media",
+    question:
+      "¿Qué desierto es el único desierto en el mundo donde el cactus; crece de forma autóctona?",
+    options: [
+      {
+        id: 2001,
+        text: "El desierto de Arabia",
+        isCorrect: false,
+      },
+      {
+        id: 2002,
+        text: "El desierto de Sonora",
+        isCorrect: true,
+      },
+      {
+        id: 2003,
+        text: "El desierto de Yuma",
+        isCorrect: false,
+      },
+      {
+        id: 2005,
+        text: "El desierto de Gobi",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 21,
+    category: "Geografía",
+    difficulty: "difícil",
+    question: "¿Cuál de estas NO es una provincia en China?",
+    options: [
+      {
+        id: 2101,
+        text: "Fujián",
+        isCorrect: false,
+      },
+      {
+        id: 2102,
+        text: "Yangtzé",
+        isCorrect: true,
+      },
+      {
+        id: 2103,
+        text: "Sichuán",
+        isCorrect: false,
+      },
+      {
+        id: 2105,
+        text: "Guangdong",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 22,
+    category: "Geografía",
+    difficulty: "difícil",
+    question: "¿Qué país no tiene salida al mar en Sudáfrica?",
+    options: [
+      {
+        id: 2201,
+        text: "Lesoto",
+        isCorrect: true,
+      },
+      {
+        id: 2202,
+        text: "Suazilandia",
+        isCorrect: false,
+      },
+      {
+        id: 2203,
+        text: "Botsuana",
+        isCorrect: false,
+      },
+      {
+        id: 2205,
+        text: "Zimbabue",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 23,
+    category: "Geografía",
+    difficulty: "difícil",
+    question:
+      "El emblema de la bandera de la República de Tayikistán presenta un amanecer sobre montañas debajo de qué símbolo?",
+    options: [
+      {
+        id: 2301,
+        text: "Pájaro",
+        isCorrect: false,
+      },
+      {
+        id: 2302,
+        text: "Hoz",
+        isCorrect: false,
+      },
+      {
+        id: 2303,
+        text: "Corona",
+        isCorrect: true,
+      },
+      {
+        id: 2305,
+        text: "Árbol",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 24,
+    category: "Geografía",
+    difficulty: "difícil",
+    question:
+      "¿En qué parte del Continente Americano se encuentra Ubicada Colombia?",
+    options: [
+      {
+        id: 2401,
+        text: "Antillas",
+        isCorrect: false,
+      },
+      {
+        id: 2402,
+        text: "Centro América",
+        isCorrect: true,
+      },
+      {
+        id: 2403,
+        text: "Sur América",
+        isCorrect: false,
+      },
+      {
+        id: 2405,
+        text: "Norte América",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: 24,
+    category: "Geografía",
+    difficulty: "difícil",
+    question: "¿Cuál es el departamento más pequeño de Colombia?",
+    options: [
+      {
+        id: 2401,
+        text: "Quindío",
+        isCorrect: true,
+      },
+      {
+        id: 2402,
+        text: "Caldas",
+        isCorrect: false,
+      },
+      {
+        id: 2403,
+        text: "Risaralda",
+        isCorrect: false,
+      },
+      {
+        id: 2405,
+        text: "San Andrés",
+        isCorrect: false,
+      },
+    ],
+  },
+];
